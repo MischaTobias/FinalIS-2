136d98a34c5193e403a1b503a329919ef2bbef9a
Mischa Tobias <mischa.tobias4b@gmail.com>
2023-05-05 19:59:32 -0600

    Candidate management

--- Final_Votaciones/sdk/order-processor/index.js
+++ Final_Votaciones/sdk/order-processor/index.js
@@ -1,6 +1,10 @@
 import { CommunicationProtocolEnum, DaprClient } from "@dapr/dapr"
 import express from 'express'
+import bodyParser from 'body-parser'
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 const apiPort = 3000
 
 // const STATE_STORES = {
@@ -49,14 +53,12 @@ switch (protocol) {
 const client = new DaprClient(host, port, protocol)
 
 app.get('/startCandidateAdding', async (req, res) => {
-
     await client.state.save(DAPR_STATE_STORE_NAME, [{ key: 'appstate', value: '1' }])
     console.log("Phase 1")
     res.send('Phase 1 initiated')
 })
 
 app.get('/finishCandidateAdding', async (req, res) => {
-
     await client.state.save(DAPR_STATE_STORE_NAME, [{ key: 'appstate', value: '2' }])
     console.log("Phase 2")
     res.send('Phase 2 initiated')
@@ -66,6 +68,55 @@ app.get('/getPhase', async (req, res) => {
     const savedPhase = await client.state.get(DAPR_STATE_STORE_NAME, 'appstate')
     console.log("Current app state: ", savedPhase)
     res.send(`Current app state: ${savedPhase}`)
+})
+
+app.get('/cleanCandidates', async (req, res) => {
+    await client.state.save(DAPR_STATE_STORE_NAME, [{ key: 'candidates', value: null }])
+    return res.send('Candidates cleared')
+})
+
+app.get('/getCandidates', async (req, res) => {
+    let currentCandidates = await client.state.get(DAPR_STATE_STORE_NAME, 'candidates')
+    if (!currentCandidates) return res.send('Sin candidatos')
+
+    console.log(JSON.stringify(currentCandidates[0].value))
+    res.send(currentCandidates[0].value)
+})
+
+app.post('/addCandidate', async (req, res) => {
+    let currentCandidates = await client.state.get(DAPR_STATE_STORE_NAME, 'candidates')
+    console.log('currentCandidates:', JSON.stringify(currentCandidates))
+    const newCandidate = req.body
+
+    if (!newCandidate.Name || !newCandidate.Party) return res.sendStatus(400)
+
+    if (currentCandidates) {
+        currentCandidates[0].value = [...currentCandidates[0].value, newCandidate]
+
+        await client.state.save(DAPR_STATE_STORE_NAME, [{ key: 'candidates', value: currentCandidates }])
+        return res.send({ response: `Candidate added`, newCandidate })
+    }
+
+    currentCandidates = [
+        {
+            key: 'candidates',
+            value: [
+                newCandidate
+            ]
+        }
+    ]
+    await client.state.save(DAPR_STATE_STORE_NAME, [{ key: 'candidates', value: currentCandidates }])
+    return res.send({ response: `Candidate added`, newCandidate })
+
+
+    // const candidates = [
+    //     {
+    //         name,
+    //         position,
+    //         party,
+    //         votes: list[ids of users]
+    //     }
+    // ]
 })
 
 
